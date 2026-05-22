bf3768c8951ac48d4b26249fb3bbae1ed734a5a1
Emi <eminolol32vg@gmail.com>
2026-05-22 14:04:58 -0700

    Asset y CMakeList

---
+++ Asset.h
@@ -0,0 +1,22 @@
+//
+// Created by Emi on 5/22/2026.
+//
+
+#ifndef PROJECTOEQUIPO_ASSET_H
+#define PROJECTOEQUIPO_ASSET_H
+
+
+#include <iostream>
+
+struct Asset {
+    std::string nombre;
+    std::string ruta;
+    std::string tipo;
+    std::string hash;
+};
+
+
+struct Collection {
+    std::string nombre;
+};
+#endif
